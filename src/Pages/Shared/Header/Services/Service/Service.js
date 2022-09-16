496845e52177bb64d73dc314a8b2ecef92ddc4ea
Habibur96 <habiburrahmannayan66@gmail.com>
2022-09-16 17:45:06 +0600

    dinamically data passed

--- src/Pages/Shared/Header/Services/Service/Service.js
+++ src/Pages/Shared/Header/Services/Service/Service.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
 const Service = ({ service }) => {
 
-    const { name, price, description, img } = service;
+    const { id, name, price, description, img } = service;
+    const navigate = useNavigate();
+    const navigateToServiceDetail = id => {
+        navigate(`/service/${id}`)
+    }
     return (
 
         <div className='service'>
@@ -11,7 +16,7 @@ const Service = ({ service }) => {
             <h3>{name}</h3>
             <p>Price: {price}</p>
             <p><small>{description}</small></p>
-            <button className='btn btn-primary'>Book: {name}</button>
+            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
         </div>
 
 
