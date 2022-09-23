684d206df568ec2365edc20b9318b566c4cc9e1f
Habibur96 <habiburrahmannayan66@gmail.com>
2022-09-23 11:26:07 +0600

    login with google added

--- src/Pages/Home/Login/Login/Register/Register.js
+++ src/Pages/Home/Login/Login/Register/Register.js
@@ -62,6 +62,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../../../firebase.init';
+import SocialLogin from '../../SocialLogin/SocialLogin';
 
 const Register = () => {
     const [
@@ -77,18 +78,22 @@ const Register = () => {
 
     const handleRegister = event => {
         event.preventDefault();
-        const name = nameRef.current.value;
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
+        // const name = nameRef.current.value;
+        // const email = emailRef.current.value;
+        // const password = passwordRef.current.value;
 
-        createUserWithEmailAndPassword(name, email, password)
-        // console.log({ name, email, password })
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        //  console.log(email, password)
+        createUserWithEmailAndPassword(email, password)
+        // console.log({ email, password })
+        // console.log({ email.email, password.password })
         // console.log("from submitted")
     }
 
     console.log(user)
     if (user) {
-        navigate('/home')
+        navigate('/login')
     }
 
 
@@ -108,23 +113,25 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                    <Form.Control type="email" placeholder="Enter email" ref={emailRef} required />
+                    <Form.Control type="email" name="email" placeholder="Enter email" ref={emailRef} required />
 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
 
-                    <Form.Control type="password" placeholder="Password" ref={passwordRef} />
+                    <Form.Control type="password" name="password" placeholder="Password" ref={passwordRef} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button onSubmit={handleRegister} variant="primary" type="submit">
+                <Button variant="primary" type="submit">
                     Submit
                 </Button>
+
             </Form>
             <br />
             <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateRegister} >Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
--- src/Pages/Home/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Home/Login/SocialLogin/SocialLogin.js
@@ -1,11 +1,31 @@
 
 
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import gglLogo from '../../../../../src/images/google.png'
-import facebook from '../../../../images/facebook.png'
-import gitHub from '../../../../images/gitHub.png'
+import auth from '../../../../firebase.init';
+import facebook from '../../../../images/facebook.png';
+import gitHub from '../../../../images/gitHub.png';
+
 
 const SocialLogin = () => {
+
+    const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+
+    let errorElement;
+    if (error) {
+
+        errorElement = <div>
+            <p className='text-danger'>Error:{error.message}</p>
+        </div>
+    }
+
+    if (user) {
+        navigate('/home')
+    }
     return (
         <div >
             <div className='d-flex align-items-center'>
@@ -13,10 +33,13 @@ const SocialLogin = () => {
                 <p className='mt-3 px-3'>Or</p>
                 <div style={{ height: '2px' }} className='bg-primary w-50'></div>
             </div>
+            {errorElement}
 
 
             <div>
-                <button className='btn btn-info w-50 d-block mx-auto my-3'>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-info w-50 d-block mx-auto my-3'>
                     <img style={{ width: '30px' }} src={gglLogo} alt="" />
                     <span className='px-2'>Google Sign In</span>
                 </button>
@@ -28,7 +51,7 @@ const SocialLogin = () => {
 
                 <button className='btn btn-info w-50 d-block mx-auto'>
 
-                    <img style={{ width: '30px' }} src={gitHub} alt="" />
+                    <img style={{ width: '45px' }} src={gitHub} alt="" />
                     <span className='px-2'>  GitHub Sign In</span>
                 </button>
             </div>
